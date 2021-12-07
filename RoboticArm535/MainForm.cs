c846cd5f30f521af0392ef7f165115e9d987e79f
Sulaiman Vali <sulaiman.vali@gmail.com>
2021-12-07 23:37:46 +0000

    - Move connect code to new class: UsbComms.

--- RoboticArm535/MainForm.cs
+++ RoboticArm535/MainForm.cs
@@ -18,17 +18,13 @@ namespace RoboticArm535
 {
     public partial class MainForm : Form
     {
-        IUsbDevice usbDevice = null;
-        private const int VendorId = 0x1267;
-        private const int ProductId = 0x0000;
-        readonly UsbSetupPacket setupPacket = new UsbSetupPacket(
-            bRequestType: 0x40, bRequest: 6, wValue: 0x100, wIndex: 0, wlength: PacketGenerator.CommandLength);
+        private UsbComms usbComms = new UsbComms();
 
         public MainForm()
         {
             InitializeComponent();
 
-            this.Text = Application.ProductName;
+            this.Text = $"{Application.ProductName} V{Application.ProductVersion}";
 
             var buttons = new Button[] {
                 button_GripOpen , button_GripClose,
@@ -57,88 +53,27 @@ namespace RoboticArm535
 
         private void checkBox_LED_CheckedChanged(object sender, EventArgs e)
         {
-            sendCommand(ControlTriggered.Led, checkBox_LED.Checked);
+            usbComms.SendCommand(ControlTriggered.Led, checkBox_LED.Checked);
         }
 
         private void Button_MouseUp(object sender, MouseEventArgs e)
         {
-            sendCommand((ControlTriggered)(sender as Button).Tag, isPressed: false);
+            usbComms.SendCommand((ControlTriggered)(sender as Button).Tag, isPressed: false);
         }
 
         private void Button_MouseDown(object sender, MouseEventArgs e)
         {
-            sendCommand((ControlTriggered)(sender as Button).Tag, isPressed: true);
-        }
-
-        private void sendCommand(ControlTriggered controlTriggered, bool isPressed)
-        {
-            if (usbDevice == null && !tryConnect())
-                return;
-
-            try
-            {
-                var buffer = PacketGenerator.GenerateFor(controlTriggered, isPressed);
-                var bytesSent = usbDevice.ControlTransfer(setupPacket, buffer, 0, buffer.Length);
-                Debug.WriteLine($"Bytes sent: {bytesSent}");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to send USB control transfer packet:\r\n" + ex.Message,
-                    Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        private bool tryConnect()
-        {
-            bool result = false;
-            try
-            {
-                using (var context = new UsbContext())
-                {
-                    context.SetDebugLevel(LibUsbDotNet.LogLevel.Info);
-
-                    //Get a list of all connected devices
-                    usbDevice = context.Find(d => d.VendorId == VendorId && d.ProductId == ProductId);
-
-                    if (usbDevice == null)
-                    {
-                        MessageBox.Show($"Unable to find USB device: VID:0x{VendorId:X4} PID:0x{ProductId:X4}.",
-                            Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        result = false;
-                    }
-                    else
-                    {
-                        //Open the device
-                        usbDevice.Open();
-
-                        //Get the first config number of the interface
-                        usbDevice.ClaimInterface(usbDevice.Configs[0].Interfaces[0].Number);
-
-                        sendCommand(ControlTriggered.Led, false);
-
-                        result = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Unable to connect to USB device:\r\n{ex.Message}.",
-                    Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                result = false;
-            }
-
-            return result;
+            usbComms.SendCommand((ControlTriggered)(sender as Button).Tag, isPressed: true);
         }
 
         private void linkLabel_Reconnect_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            tryConnect();
+            usbComms.TryConnect();
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (usbDevice != null && usbDevice.IsOpen)
-                usbDevice.Close();
+            usbComms.Close();
         }
     }
 }
