88937d97dd9f83a4638e0be82884e10c8be1ba80
Sulaiman Vali <sulaiman.vali@gmail.com>
2021-12-11 02:57:35 +0000

    Make the robotic arm clap hands, lol.

--- RoboticArm535Console/Program.cs
+++ RoboticArm535Console/Program.cs
@@ -6,9 +6,10 @@ namespace RoboticArm535Console
 {
     class Program
     {
+        static readonly UsbComms usbComms = new UsbComms();
+
         static void Main(string[] args)
         {
-            var usbComms = new UsbComms();
             if (usbComms.Connect() != UsbConnErrorCode.NoError)
             {
                 Console.WriteLine("Unable to connect to USB device.");
@@ -16,23 +17,42 @@ namespace RoboticArm535Console
                 return;
             }
 
+            Console.WriteLine("Press any key to abort script");
+
             // demonstrates a simple script controlling multiple outputs 
             usbComms.SendCommandMulti(Outputs.Led.On, Outputs.Grip.Stop, Outputs.Wrist.Stop, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
-            Thread.Sleep(500);
-            usbComms.SendCommandMulti(Outputs.Led.Off, Outputs.Grip.Open, Outputs.Wrist.Stop, Outputs.Elbow.Up, Outputs.Stem.Stop, Outputs.Base.Stop);
-            Thread.Sleep(500);
+            wait(500);
+            usbComms.SendCommandMulti(Outputs.Led.Off, Outputs.Grip.Open, Outputs.Wrist.Up, Outputs.Elbow.Up, Outputs.Stem.Stop, Outputs.Base.Stop);
+            wait(3000);
             usbComms.SendCommandMulti(Outputs.Led.On, Outputs.Grip.Stop, Outputs.Wrist.Up, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
-            Thread.Sleep(500);
+            wait(500);
             usbComms.SendCommandMulti(Outputs.Led.Off, Outputs.Grip.Stop, Outputs.Wrist.Stop, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
-            Thread.Sleep(500);
+            wait(500);
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 7; i++)
             {
                 usbComms.SendCommandMulti(Outputs.Led.On, Outputs.Grip.Open, Outputs.Wrist.Down, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
-                Thread.Sleep(3000);
+                wait(800);
                 usbComms.SendCommandMulti(Outputs.Led.Off, Outputs.Grip.Close, Outputs.Wrist.Up, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
-                Thread.Sleep(3000);
+                wait(800);
             }
+            stopAll();
+        }
+        
+
+        private static void wait(int millisecondsTimeout)
+        {
+            if (Console.KeyAvailable)
+            {
+                stopAll();
+                Environment.Exit(0);
+            }
+
+            Thread.Sleep(millisecondsTimeout);
+        }
+
+        private static void stopAll()
+        {
             usbComms.SendCommandMulti(Outputs.Led.Off, Outputs.Grip.Stop, Outputs.Wrist.Stop, Outputs.Elbow.Stop, Outputs.Stem.Stop, Outputs.Base.Stop);
         }
     }
