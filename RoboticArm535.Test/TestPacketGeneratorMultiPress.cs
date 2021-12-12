a0c304e6b545ab4bc3de93dc40f1e6239121cd59
Sulaiman Vali <sulaiman.vali@gmail.com>
2021-12-12 20:01:49 +0000

    Refactor unit tests.

--- RoboticArm535.Test/TestPacketGeneratorMultiPress.cs
+++ RoboticArm535.Test/TestPacketGeneratorMultiPress.cs
@@ -6,40 +6,38 @@ namespace RoboticArm535.Test
     [TestClass]
     public class TestPacketGeneratorMultiPress
     {
+        private void checkPacket(byte[] actual, byte[] expected)
+        {
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual<byte>(expected[i], actual[i]);
+        }
+
         [TestMethod]
         public void GenMultiPress_AllOff_Ok()
         {
             var bytes = PacketGenerator.GenMultiPress(Out.Led.Off, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
-            var expected = new byte[] { 0, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0, 0 });
         }
 
         [TestMethod]
         public void GenMultiPress_LedOn_Ok()
         {
             var bytes = PacketGenerator.GenMultiPress(Out.Led.On, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
-            var expected = new byte[] { 0, 0, 0x01 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0, 0x01 });
         }
 
         [TestMethod]
         public void GenMultiPress_OnCloseUpAheadRight_Ok()
         {
             var bytes = PacketGenerator.GenMultiPress(Out.Led.On, Out.Grip.Close, Out.Wrist.Up, Out.Elbow.Up, Out.Stem.Ahead, Out.Base.Right);
-            var expected = new byte[] { 0x95, 0x01, 0x01 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x95, 0x01, 0x01 });
         }
 
         [TestMethod]
         public void GenMultiPress_OffOpenDownBackLeft_Ok()
         {
             var bytes = PacketGenerator.GenMultiPress(Out.Led.Off, Out.Grip.Open, Out.Wrist.Down, Out.Elbow.Down, Out.Stem.Back, Out.Base.Left);
-            var expected = new byte[] { 0x6a, 0x02, 0x00 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x6a, 0x02, 0x00 });
         }
     }
 }
--- RoboticArm535.Test/TestPacketGeneratorSinglePress.cs
+++ RoboticArm535.Test/TestPacketGeneratorSinglePress.cs
@@ -6,112 +6,94 @@ namespace RoboticArm535.Test
     [TestClass]
     public class TestPacketGeneratorSinglePress
     {
+        private void checkPacket(byte[] actual, byte[] expected)
+        {
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual<byte>(expected[i], actual[i]);
+        }
+
         [TestMethod]
         public void GenSinglePress_LedOn_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.LedOn);
-            var expected = new byte[] { 0, 0, 0x01 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0, 0x01 });
         }
 
         [TestMethod]
         public void GenSinglePress_LedOff_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.AllOff);
-            var expected = new byte[] { 0, 0, 0x00 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0, 0x00 });
         }
 
         [TestMethod]
         public void GenSinglePress_GripClose_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.GripClose);
-            var expected = new byte[] { 0x01, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x01, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_GripOpen_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.GripOpen);
-            var expected = new byte[] { 0x02, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x02, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_WristUp_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.WristUp);
-            var expected = new byte[] { 0x04, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x04, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_WristDown_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.WristDown);
-            var expected = new byte[] { 0x08, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x08, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_ElbowUp_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.ElbowUp);
-            var expected = new byte[] { 0x10, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x10, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_ElbowDown_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.ElbowDown);
-            var expected = new byte[] { 0x20, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x20, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_StemBack_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.StemBack);
-            var expected = new byte[] { 0x40, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x40, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_StemAhead_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.StemAhead);
-            var expected = new byte[] { 0x80, 0, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0x80, 0, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_BaseRight_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.BaseRight);
-            var expected = new byte[] { 0, 0x01, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0x01, 0 });
         }
 
         [TestMethod]
         public void GenSinglePress_BaseLeft_Ok()
         {
             var bytes = PacketGenerator.GenSinglePress(OpCode.BaseLeft);
-            var expected = new byte[] { 0, 0x02, 0 };
-            for (int i = 0; i < bytes.Length; i++)
-                Assert.AreEqual<byte>(expected[i], bytes[i]);
+            checkPacket(bytes, new byte[] { 0, 0x02, 0 });
         }
     }
 }
