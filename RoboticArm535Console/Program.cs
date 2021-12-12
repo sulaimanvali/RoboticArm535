e538424a344123afb9c288bd5d96084866a48cad
Sulaiman Vali <sulaiman.vali@gmail.com>
2021-12-12 00:47:48 +0000

    Add code to demonstrate even simpler script.

--- RoboticArm535Console/Program.cs
+++ RoboticArm535Console/Program.cs
@@ -19,9 +19,10 @@ namespace RoboticArm535Console
 
             Console.WriteLine("Press any key to abort script");
 
-            // demonstrates a simple script controlling multiple outputs simultaneously
-            sendCommandsByOutputs();
+            // demonstrates a simple script controlling multiple outputs simultaneously in different ways
+            //sendCommandsByOutputs();
             //sendCommandsByBytes();
+            sendCommandsByTimedOpCodeMasks();
         }
 
         private static void sendCommandsByOutputs()
@@ -29,13 +30,13 @@ namespace RoboticArm535Console
             usb.Cmd(Out.Led.On, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
             wait(500);
             usb.Cmd(Out.Led.Off, Out.Grip.Open, Out.Wrist.Up, Out.Elbow.Up, Out.Stem.Stop, Out.Base.Stop);
-            wait(2000);
+            wait(1000);
             usb.Cmd(Out.Led.On, Out.Grip.Stop, Out.Wrist.Up, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
             wait(500);
             usb.Cmd(Out.Led.Off, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
             wait(500);
 
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < 5; i++)
             {
                 usb.Cmd(Out.Led.On, Out.Grip.Open, Out.Wrist.Down, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
                 wait(800);
@@ -54,12 +55,28 @@ namespace RoboticArm535Console
             usb.Cmd(Packet.Byte0.ArmStop, Packet.Byte1.BaseStop, Packet.Byte2.LedOn);
             wait(500);
 
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < 5; i++)
             {
                 usb.Cmd(Packet.Byte0.GripOpen | Packet.Byte0.WristUp | Packet.Byte0.ElbowUp, Packet.Byte1.BaseStop, Packet.Byte2.LedOff);
                 wait(800);
                 usb.Cmd(Packet.Byte0.GripClose | Packet.Byte0.WristDown | Packet.Byte0.ElbowDown, Packet.Byte1.BaseStop, Packet.Byte2.LedOn);
                 wait(800);
+            }
+            stopAll();
+        }
+
+        private static void sendCommandsByTimedOpCodeMasks()
+        {
+            usb.TurnLedOn(true);
+            usb.Cmd(OpCode.WristUp, 1.0f);
+            usb.Cmd(OpCode.ElbowUp, 1.0f);
+
+            for (int i = 0; i < 5; i++)
+            {
+                usb.Cmd(OpCode.GripOpen | OpCode.WristUp, 0.8f);
+                usb.TurnLedOn(true);
+                usb.Cmd(OpCode.GripClose | OpCode.WristDown, 0.8f);
+                usb.TurnLedOn(false);
             }
             stopAll();
         }
