22aaab4749a256f646505b3aa8e434c0645937e7
Sulaiman Vali <sulaiman.vali@gmail.com>
2022-01-01 17:20:18 +0000

    Swap argument positions to match Assert method.

--- RoboticArm535.Test/TestPacketGeneratorByOpCode.cs
+++ RoboticArm535.Test/TestPacketGeneratorByOpCode.cs
@@ -7,7 +7,7 @@ namespace RoboticArm535.Test
     [TestClass]
     public class TestPacketGeneratorByOpCode
     {
-        private static void checkPacket(byte[] actual, byte[] expected)
+        private static void checkPacket(byte[] expected, byte[] actual)
         {
             for (int i = 0; i < actual.Length; i++)
                 Assert.AreEqual<byte>(expected[i], actual[i]);
@@ -17,91 +17,91 @@ namespace RoboticArm535.Test
         public void GenByOpCode_LedOn_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.LedOn);
-            checkPacket(bytes, new byte[] { 0, 0, 0x01 });
+            checkPacket(new byte[] { 0, 0, 0x01 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_LedOff_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.LedOff);
-            checkPacket(bytes, new byte[] { 0, 0, 0x00 });
+            checkPacket(new byte[] { 0, 0, 0x00 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_GripClose_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.GripClose);
-            checkPacket(bytes, new byte[] { 0x01, 0, 0 });
+            checkPacket(new byte[] { 0x01, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_GripOpen_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.GripOpen);
-            checkPacket(bytes, new byte[] { 0x02, 0, 0 });
+            checkPacket(new byte[] { 0x02, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_WristUp_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.WristUp);
-            checkPacket(bytes, new byte[] { 0x04, 0, 0 });
+            checkPacket(new byte[] { 0x04, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_WristDown_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.WristDown);
-            checkPacket(bytes, new byte[] { 0x08, 0, 0 });
+            checkPacket(new byte[] { 0x08, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_ElbowUp_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.ElbowUp);
-            checkPacket(bytes, new byte[] { 0x10, 0, 0 });
+            checkPacket(new byte[] { 0x10, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_ElbowDown_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.ElbowDown);
-            checkPacket(bytes, new byte[] { 0x20, 0, 0 });
+            checkPacket(new byte[] { 0x20, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_StemBack_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.StemBack);
-            checkPacket(bytes, new byte[] { 0x40, 0, 0 });
+            checkPacket(new byte[] { 0x40, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_StemAhead_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.StemAhead);
-            checkPacket(bytes, new byte[] { 0x80, 0, 0 });
+            checkPacket(new byte[] { 0x80, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_BaseRight_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.BaseRight);
-            checkPacket(bytes, new byte[] { 0, 0x01, 0 });
+            checkPacket(new byte[] { 0, 0x01, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_BaseLeft_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.BaseLeft);
-            checkPacket(bytes, new byte[] { 0, 0x02, 0 });
+            checkPacket(new byte[] { 0, 0x02, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOpCode_AllOff_Ok()
         {
             var bytes = PacketGenerator.GenByOpCode(OpCode.AllOff);
-            checkPacket(bytes, new byte[] { 0, 0, 0 });
+            checkPacket(new byte[] { 0, 0, 0 }, bytes);
         }
 
         [TestMethod]
--- RoboticArm535.Test/TestPacketGeneratorByOutputs.cs
+++ RoboticArm535.Test/TestPacketGeneratorByOutputs.cs
@@ -6,7 +6,7 @@ namespace RoboticArm535.Test
     [TestClass]
     public class TestPacketGeneratorByOutputs
     {
-        private static void checkPacket(byte[] actual, byte[] expected)
+        private static void checkPacket(byte[] expected, byte[] actual)
         {
             for (int i = 0; i < actual.Length; i++)
                 Assert.AreEqual<byte>(expected[i], actual[i]);
@@ -16,28 +16,28 @@ namespace RoboticArm535.Test
         public void GenByOutputs_LedOff_Ok()
         {
             var bytes = PacketGenerator.GenByOutputs(Out.Led.Off, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
-            checkPacket(bytes, new byte[] { 0, 0, 0 });
+            checkPacket(new byte[] { 0, 0, 0 }, bytes);
         }
 
         [TestMethod]
         public void GenByOutputs_LedOn_Ok()
         {
             var bytes = PacketGenerator.GenByOutputs(Out.Led.On, Out.Grip.Stop, Out.Wrist.Stop, Out.Elbow.Stop, Out.Stem.Stop, Out.Base.Stop);
-            checkPacket(bytes, new byte[] { 0, 0, 0x01 });
+            checkPacket(new byte[] { 0, 0, 0x01 }, bytes);
         }
 
         [TestMethod]
         public void GenByOutputs_OnCloseUpAheadRight_Ok()
         {
             var bytes = PacketGenerator.GenByOutputs(Out.Led.On, Out.Grip.Close, Out.Wrist.Up, Out.Elbow.Up, Out.Stem.Ahead, Out.Base.Right);
-            checkPacket(bytes, new byte[] { 0x95, 0x01, 0x01 });
+            checkPacket(new byte[] { 0x95, 0x01, 0x01 }, bytes);
         }
 
         [TestMethod]
         public void GenByOutputs_OffOpenDownBackLeft_Ok()
         {
             var bytes = PacketGenerator.GenByOutputs(Out.Led.Off, Out.Grip.Open, Out.Wrist.Down, Out.Elbow.Down, Out.Stem.Back, Out.Base.Left);
-            checkPacket(bytes, new byte[] { 0x6a, 0x02, 0x00 });
+            checkPacket(new byte[] { 0x6a, 0x02, 0x00 }, bytes);
         }
     }
 }
