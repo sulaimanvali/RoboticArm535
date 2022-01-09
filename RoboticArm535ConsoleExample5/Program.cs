b9df956fe1a40322a77f1b161d2568e1f8e323ec
Sulaiman Vali <sulaiman.vali@gmail.com>
2022-01-09 02:52:16 +0000

    Add console example 5 to demonstate an even simpler script (but with no aborting capability).

---
+++ RoboticArm535ConsoleExample5/Program.cs
@@ -0,0 +1,28 @@
+﻿using RoboticArm535Library;
+using System;
+
+UsbComms usb = new();
+if (usb.Connect() != UsbConnErrorCode.NoError)
+{
+    Console.WriteLine("Unable to connect to USB device.");
+    return;
+}
+
+var script =
+@"
+WristUp 0.65
+ElbowUp 1.05
+GripOpen 0.92
+GripClose 0.85
+LedOn 0.3
+GripOpen 1
+LedOn 0.3
+GripClose 1
+LedOn 0.3
+GripOpen 1
+LedOn 0.3
+GripClose 1
+LedOn 0.3
+WristDown 0.80
+ElbowDown 0.77";
+await usb.RunScript(script, new Progress<int>(i => Console.WriteLine($"Line {i}")));
